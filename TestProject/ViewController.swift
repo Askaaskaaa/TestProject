48af90799b494ca3a87fd5b2a9817098c95676a8
Asylzhan <yeldes.asylzhan@gmail.com>
2023-06-02 22:48:09 +0600

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Asylzhan Yeldes on 02.06.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
